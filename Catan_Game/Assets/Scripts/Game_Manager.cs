124f4b12d29e8450c5d7320246da609eb6e6bd6b
ThomasKearney <thomasnkearney@gmail.com>
2018-02-07 21:34:04 -0600

    Adding some more empty classes / testing gitignore

--- Catan_Game/Assets/Scripts/Game_Manager.cs
+++ Catan_Game/Assets/Scripts/Game_Manager.cs
@@ -2,17 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Game : MonoBehaviour {
-
+public class Game : MonoBehaviour
+{
+    // player who's turn it is currently
+    int current_player;
+    Player p;
 
 
 	// Use this for initialization
-	void Start () {
-		
-	}
+	void Start ()
+    {
+        current_player = 1;
+        //TODO 
+        // 4 needs to be eventually replaced by the number of players that the user selects
+        number_of_players = 4;
+        for(int i = 1; i < number_of_players; i++)
+        {
+
+        }
+
+    }
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
 }
