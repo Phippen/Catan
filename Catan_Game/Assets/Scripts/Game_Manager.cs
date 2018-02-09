37bf3e95aa09480205fe897f1442c662b19e982a
ThomasKearney <thomasnkearney@gmail.com>
2018-02-08 21:13:19 -0600

    Added a tile array to be created at the biginning of the game.

--- Catan_Game/Assets/Scripts/Game_Manager.cs
+++ Catan_Game/Assets/Scripts/Game_Manager.cs
@@ -1,19 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Game_Manager : MonoBehaviour
 {
     // Constants for assets
-    private readonly int wheat = 1;
-    private readonly int log   = 2;
-    private readonly int brick = 3;
-    private readonly int stone = 4;
-    private readonly int wool  = 5;
+    static public readonly int wheat = 1;
+    static public readonly int log   = 2;
+    static public readonly int brick = 3;
+    static public readonly int stone = 4;
+    static public readonly int wool  = 5;
+    static public readonly int desert  = 6;
 
     // player who's turn it is currently
     int current_player;
-    Intersection[] intersections = Create_Intersections(); 
+    Intersection[] intersections = Create_Intersections();
+    int[,] tiles = Create_Tile_List();
 
     // for future reference, order of the tokens
     // 5 2 6 3 8 10 9 12 11 4 8 10 9 4 5 6 3 11
@@ -30,6 +33,13 @@ public class Game_Manager : MonoBehaviour
 
         }
 
+        for(int i = 0; i < 20; i++)
+        {
+            Console.Write(tiles[i, 0]);
+            Console.Write(", ");
+            Console.Write(tiles[i, 1] + "\n");
+        }
+
     }
 
     // Update is called once per frame
@@ -38,32 +48,110 @@ public class Game_Manager : MonoBehaviour
 
     }
 
+    static private int[,] Create_Tile_List()
+    {
+        // List of of dice roll tokens in order
+        int[] tokens = { 5, 2, 6, 3, 8, 1, 0, 9, 12, 11, 4, 8, 10, 9, 4, 5, 6, 3, 11 };
+        // List of possible yields
+        int[] yields = { wheat, wheat, wheat, wheat, log, log, log, log, brick, brick, brick, stone, stone, stone, wool, wool, wool, wool, desert};
+        // Make it random so we can insert it into the board
+        yields = randomize(yields);
+
+        // 19 tiles, first column is the dice roll token it has, second is its yield
+        int[,] tiles = new int[19, 2];
+
+        int j = 0;
+        for(int i = 0; i < 19; i++)
+        {
+            tiles[i, 1] = yields[i];
+            if(yields[i] != 6)
+            {
+                tiles[i, 0] = tokens[j];
+                j++;
+            }
+        }
+
+        return tiles;
+    }
+
     static private Intersection[] Create_Intersections()
     {
-        Intersection[] new_intersections = new Intersection[59];
+        Intersection[] new_intersections = new Intersection[54];
 
         // Creating a list of the order that neighbors will be arranged in. If you can
         // think of better way to make this less hideous then please do. But I thought
         // This was better than 59 lines of calling new Intersection
 
-        // 0 will represent neighbors that do not exist for costal cities
-        int[] neighbors = { 4, 0, 5, 5, 0, 6, 6, 0, 7, 0, 8, 1, 1, 9, 2, 2, 10, 3, 7, 11, 0, 12, 4, 13, 13, 5, 14, 14, 6, 15, 15, 7, 16, 22, 12, 23, 23, 13, 24, 24, 14, 25, 15, 15, 16, 16, 16, 27, 0, 28, 17, 17, 29, 18, 18, 30, 19, 19, 31, 20, 20, 32, 21, 21, 33, 0, 0, 22, 34, 34, 23, 35, 35, 24, 36, 36, 25, 37, 37, 26, 38, 38, 27, 0, 0, 34, 44, 44, 35, 45, 45, 36, 46, 46, 37, 47, 47, 38, 0, 0, 44, 52, 52, 45, 53, 53, 46, 54, 54, 47, 0, 48, 0, 47, 49, 0, 50, 50, 0, 51 };
-
-        int neighbor_count = 0;
-        for( int intersection = 1; intersection < 55; intersection++)
+        // 0 will represent neighbors that do not exist for coastal cities
+        int[,] neighbors = { { 4, 0, 5 },
+                             { 5, 0, 6 },
+                             { 6, 0, 7 },
+                             { 0, 8, 1 },
+                             { 1, 9, 2 },
+                             { 2, 10, 3 },
+                             { 7, 11, 0 },
+                             { 12, 4, 13 },
+                             { 13, 5, 14 },
+                             { 14, 6, 15 },
+                             { 15, 7, 16 },
+                             { 22, 12, 23 },
+                             { 23, 13, 24 },
+                             { 24, 14, 25 },
+                             { 15, 15, 16 },
+                             { 16, 16, 27 },
+                             { 0, 28, 17 },
+                             { 17, 29, 18 },
+                             { 18, 30, 19 },
+                             { 19, 31, 20 },
+                             { 20, 32, 21 },
+                             { 21, 33, 0 },
+                             { 0, 22, 34 },
+                             { 34, 23, 35 },
+                             { 35, 24, 36 },
+                             { 36, 25, 37 },
+                             { 37, 26, 38 },
+                             { 38, 27, 0 },
+                             { 0, 34, 44 },
+                             { 44, 35, 45 },
+                             { 45, 36, 46 },
+                             { 46, 37, 47 },
+                             { 47, 38, 0 },
+                             { 0, 44, 52 },
+                             { 52, 45, 53 },
+                             { 53, 46, 54 },
+                             { 54, 47, 0 },
+                             { 48, 0, 47 },
+                             { 49, 0, 50 },
+                             { 50, 0, 51 } };
+        
+        for(int intersection = 0; intersection < 54; intersection++)
         {
-            int[] nieghbors_to_add =
+            int[] neighbors_to_add =
             {
-                neighbors[neighbor_count],
-                neighbors[neighbor_count + 1],
-                neighbors[neighbor_count + 2]
+                neighbors[intersection, 0],
+                neighbors[intersection, 1],
+                neighbors[intersection, 2]
             };
 
-            new_intersections[intersection] = new Intersection(neighbors, intersection);
-            neighbor_count += 3;
+            new_intersections[intersection] = new Intersection(neighbors_to_add, intersection);
         }
         
         return new_intersections;
     }
+
+    static private int[] randomize(int[] given_array)
+    {
+        System.Random rng = new System.Random();
+        int n = given_array.Length;
+        for(n = given_array.Length; n > 1; n--)
+        {
+            int k = rng.Next(n + 1);
+            int value = given_array[k];
+            given_array[k] = given_array[n];
+            given_array[n] = value;
+        }
+        return given_array;
+     }
+    
 
 }
--- Catan_Game/Assets/Scripts/Intersection.cs
+++ Catan_Game/Assets/Scripts/Intersection.cs
@@ -10,6 +10,10 @@ public class Intersection : MonoBehaviour {
     // The neighbors will read left to right as in 0 place will be the bottom or upper left 
     // node, 1 will be upper or lower, and 2 will be upper rigth or lower right
     private int[] neighbors;
+    // Doubley indexed array of 6 ints that represents yield for a node
+    // First column represents the dice roll token that is connected to that node
+    // Second column represents the yield that the dice roll is tied to
+    private int[][] rewards;
 
     public Intersection(int[] init_neighbors, int init_id)
     {
