a2469c384b88ce034908d4c3003bcc7184b64605
ThomasKearney <thomasnkearney@gmail.com>
2018-06-20 18:19:08 -0500

    wasn't using anything from monodevelop so there was no reason to be extending it.

--- Catan_Game/Assets/Scripts/Intersection.cs
+++ Catan_Game/Assets/Scripts/Intersection.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Intersection : MonoBehaviour {
+public class Intersection {
 
     // Every intersecion is given a numberical intersection
     private int id;
