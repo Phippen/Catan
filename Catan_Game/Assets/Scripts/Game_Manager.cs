3740020bb7ccc06e01e493ecf0fcbdc41fa88b3d
ThomasKearney <thomasnkearney@gmail.com>
2018-06-20 18:22:49 -0500

    worked out kinks in board data structure. corrected neighbors array. fixed array looping index issues (out of bounds)

--- Catan_Game/Assets/Scripts/Game_Manager.cs
+++ Catan_Game/Assets/Scripts/Game_Manager.cs
@@ -27,6 +27,7 @@ public class Game_Manager : MonoBehaviour
     // Use this for initialization
     void Start()
     {
+        Debug.Log("Game Starting");
         current_player = 1;
         //TODO 
         // 4 needs to be eventually replaced by the number of players that the user selects
@@ -36,11 +37,9 @@ public class Game_Manager : MonoBehaviour
 
         }
 
-        for(int i = 0; i < 20; i++)
+        for(int i = 0; i < 19; i++)
         {
-            Console.Write(tiles[i, 0]);
-            Console.Write(", ");
-            Console.Write(tiles[i, 1] + "\n");
+            Debug.Log(tiles[i, 0] + ", " + tiles[i, 1]);
         }
 
         for (int i = 2; i < 13; i++)
@@ -93,46 +92,60 @@ public class Game_Manager : MonoBehaviour
         // This was better than 59 lines of calling new Intersection
 
         // 0 will represent neighbors that do not exist for coastal cities
-        int[,] neighbors = { { 4, 0, 5 },
-                             { 5, 0, 6 },
-                             { 6, 0, 7 },
-                             { 0, 8, 1 },
-                             { 1, 9, 2 },
-                             { 2, 10, 3 },
-                             { 7, 11, 0 },
-                             { 12, 4, 13 },
-                             { 13, 5, 14 },
-                             { 14, 6, 15 },
-                             { 15, 7, 16 },
-                             { 22, 12, 23 },
-                             { 23, 13, 24 },
-                             { 24, 14, 25 },
-                             { 15, 15, 16 },
-                             { 16, 16, 27 },
-                             { 0, 28, 17 },
-                             { 17, 29, 18 },
-                             { 18, 30, 19 },
-                             { 19, 31, 20 },
-                             { 20, 32, 21 },
-                             { 21, 33, 0 },
-                             { 0, 22, 34 },
-                             { 34, 23, 35 },
-                             { 35, 24, 36 },
-                             { 36, 25, 37 },
-                             { 37, 26, 38 },
-                             { 38, 27, 0 },
-                             { 0, 34, 44 },
-                             { 44, 35, 45 },
-                             { 45, 36, 46 },
-                             { 46, 37, 47 },
-                             { 47, 38, 0 },
-                             { 0, 44, 52 },
-                             { 52, 45, 53 },
-                             { 53, 46, 54 },
-                             { 54, 47, 0 },
-                             { 48, 0, 47 },
-                             { 49, 0, 50 },
-                             { 50, 0, 51 } };
+        int[,] neighbors = { { 4, 0, 5 },    //1
+                             { 5, 0, 6 },    //2
+                             { 6, 0, 7 },    //3
+                             { 0, 8, 1 },    //4
+                             { 1, 9, 2 },    //5
+                             { 2, 10, 3 },   //6
+                             { 7, 11, 0 },   //7
+                             { 12, 4, 13 },  //8
+                             { 13, 5, 14 },  //9
+                             { 14, 6, 15 },  //10
+                             { 15, 7, 16 },  //11
+                             { 22, 12, 23 }, //12
+                             { 23, 13, 24 }, //13
+                             { 24, 14, 25 }, //14
+                             { 10, 20, 11 }, //15
+                             { 11, 21, 0 },  //16
+                             { 22, 12, 23 }, //17
+                             { 23, 13, 24 }, //18
+                             { 24, 14, 25 }, //19
+                             { 25, 15, 26 }, //20
+                             { 26, 16, 27 }, //21
+                             { 0, 28, 27 },  //22
+                             { 17, 29, 18 }, //23
+                             { 18, 30, 19 }, //24
+                             { 19, 31, 20 }, //25
+                             { 20, 32, 21 }, //26
+                             { 21, 33, 0 },  //27
+                             { 0, 22, 34 },  //28
+                             { 34, 23, 35 }, //29
+                             { 35, 24, 36 }, //30
+                             { 36, 25, 37 }, //31
+                             { 37, 26, 38 }, //32
+                             { 38, 27, 0 },  //33
+                             { 28, 39, 29 }, //34
+                             { 29, 40, 0 },  //35
+                             { 30, 41, 31 }, //36
+                             { 31, 42, 32 }, //37
+                             { 32, 43, 33 }, //38
+                             { 0, 34, 44 },  //39
+                             { 44, 35, 45 }, //40
+                             { 45, 36, 46 }, //41
+                             { 46, 37, 47 }, //42
+                             { 47, 38, 0 },  //43
+                             { 39, 48, 40 }, //44
+                             { 40, 49, 41 }, //45
+                             { 41, 50, 42 }, //46
+                             { 42, 51, 43 }, //47
+                             { 0, 44, 52 },  //48
+                             { 52, 45, 53 }, //49
+                             { 53, 46, 54 }, //50
+                             { 54, 47, 0 },  //51
+                             { 48, 0, 47 },  //52
+                             { 49, 0, 50 },  //53
+                             { 50, 0, 51 } };//54
         
         for(int intersection = 0; intersection < 54; intersection++)
         {
@@ -251,10 +264,10 @@ public class Game_Manager : MonoBehaviour
     static private int[] Randomize(int[] given_array)
     {
         System.Random rng = new System.Random();
-        int n = given_array.Length;
-        for(n = given_array.Length; n > 1; n--)
+        int n = given_array.Length - 1;
+        for(n = given_array.Length-1; n > -1; n--)
         {
-            int k = rng.Next(n + 1);
+            int k = rng.Next(n);
             int value = given_array[k];
             given_array[k] = given_array[n];
             given_array[n] = value;
